5cfd4dfab52658388dcf75b48f03f6b49662b42d
Jolson Eric Cruz <2219359@edu.vaniercollege.qc.ca>
2023-05-24 11:13:05 -0400

    Added wpfCar

---
+++ May 24/wpfCarBind/wpfCarBind/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+
+namespace wpfCarBind
+{
+    public partial class MainWindow : Window
+    {
+        public List<wpfCar> MyCars
+        {
+            get;
+            set;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            wpfCar Car1 = new wpfCar("Toyota", 2010);
+            wpfCar Car2 = new wpfCar("Nissan", 2014);
+            wpfCar Car3 = new wpfCar("Cadillac", 2015);
+
+            MyCars = new List<wpfCar>();
+
+            MyCars.Add(Car1);
+            MyCars.Add(Car2);
+            MyCars.Add(Car3);
+
+            DataContext = this;
+        }
+    }
+}
---
+++ May 24/wpfCarBind/wpfCarBind/wpfCar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace wpfCarBind
+{
+    public class wpfCar
+    {
+
+        public string Brand {
+            get;
+            set;
+        }
+
+        public int Year {
+            get;
+            set;
+        }
+
+        public wpfCar()
+        {
+            this.Brand = "";
+            this.Year = 0000;
+        }
+
+        public wpfCar(String Brand, int Year)
+        {
+            this.Brand = Brand;
+            this.Year = Year;
+        }
+    }
+}
