4dcadc26b5a663450e232abeaad3ccaee7557222
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2023-06-03 22:25:39 -0400

    Updated new files

---
+++ Lab2/wpDistanceTraveled/wpDistanceTraveled/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace wpDistanceTraveled
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private double var1 = 0;
+        private double var2 = 0;
+        private double var3 = 0;
+        private String str = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PullInput()
+        {
+            var1 = double.Parse(input1.Text);
+            var2 = double.Parse(input2.Text);
+        }
+
+        public void CalculateDistance()
+        {
+            var3 = var1 * var2;
+        }
+        private void calculateResult()
+        {
+            for (int i = 1; i <= Math.Ceiling(var2); i++)
+            {
+                str += "Hour " + i + ": Travelled " + var1 * i + " miles!\n";
+            }
+        }
+
+        private void resultBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PullInput();
+            result.Clear();
+            calculateResult();
+            Console.WriteLine(str);
+            result.Text = str;
+        }
+
+    }
+}
---
+++ Lab2/wpfShippingCharges/wpfShippingCharges/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace wpfShippingCharges
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private double var1 = 0;
+        private double var2 = 0;
+        private String str = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PullInput()
+        {
+            var1 = double.Parse(input1.Text);
+            var2 = double.Parse(input2.Text);
+        }
+
+        private String calculateResult()
+        {
+            double charges = 0.0;
+
+            if (var1 <= 2.0)
+                charges = Math.Ceiling(var2 / 500) * 1.10;
+            else if (var1 < 6.0)
+                charges = Math.Ceiling(var2 / 500) * 2.20;
+            else if (var1 < 10.0)
+                charges = Math.Ceiling(var2 / 500) * 3.70;
+            else
+                charges = Math.Ceiling(var2 / 500) * 4.80;
+
+            if (charges == 0.0)
+            {
+                return "Error! Fill-in all parameters!";
+            }
+            else
+            {
+                return "The total calculated fee is: $" + charges;
+            }
+        }
+
+        private void resultBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PullInput();
+            str = calculateResult();
+            result.Content = str;
+        }
+    }
+}
