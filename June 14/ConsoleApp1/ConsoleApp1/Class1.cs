09f3d7444c0e1faf3cfaf76b81945234542df5c5
Jolson Eric Cruz <2219359@edu.vaniercollege.qc.ca>
2023-06-14 11:39:05 -0400

    UnitTesting

---
+++ June 14/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace UnitTesting
+{
+    class Class1
+    {
+        [Test]
+        public void TestAdd()
+        {
+            //Arrange
+            Program program = new Program();
+
+            // Act
+            double result = program.add(2, 9.2);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(11.2));
+        }
+
+        [Test]
+        public void TestMinus()
+        {
+            //Arrange
+            Program program = new Program();
+
+            // Act
+            double result = program.minus(9.2, 2);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(7.2));
+        }
+
+    }
+}
